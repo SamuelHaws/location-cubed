314724d4f56521e671c99f0d99aef0232adc5dbb
SamuelHaws <samhaws33@gmail.com>
2020-04-16 20:57:22 -0400

    add /unique GET endpoint

--- api/app.py
+++ api/app.py
@@ -15,5 +15,10 @@ def index():
 def test():
   return json.dumps(client.get("d6g9-xbgu", limit=200, content_type="json"))
 
+# Get the unique descripts for business licenses
+@app.route('/unique')
+def unique():
+  return json.dumps(client.get("qcyy-feh8", limit=200, content_type="json", select="distinct(descript)"))
+
 if __name__ == '__main__':
   app.run(debug=True)
