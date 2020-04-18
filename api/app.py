3467ba20cad9eca6983b2e856cc19a5cd98d5480
Colum Cross <cxc9401@rit.edu>
2020-04-18 15:41:38 -0400

    Adds the basis for the score calculation algo.

--- api/app.py
+++ api/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from sodapy import Socrata
-import config
-import json
+from flask import request
 from flask_cors import CORS
+
+from sodapy import Socrata
+
+import json
+import config
+import engine
 
 app = Flask(__name__)
 CORS(app)
@@ -21,6 +25,13 @@ def test():
 @app.route('/unique')
 def unique():
   return json.dumps(client.get("qcyy-feh8", limit=200, content_type="json", select="distinct(descript)"))
+
+@app.route('/getscorebyaddress')
+def getscorebyaddress():
+  housenumber = request.args.get('houseno', type = int)
+  street = request.args.get('street', type = str)
+  business = request.args.get('businessType', type = str)
+  return str(engine.generatescorefromaddress(housenumber, street, business))
 
 if __name__ == '__main__':
   app.run(debug=True)
---
+++ api/engine.py
@@ -0,0 +1,48 @@
+from sodapy import Socrata
+import config
+opendatabuffalo = Socrata("data.buffalony.gov", config.APP_TOKEN)
+
+def checkifzonedforcommercial(codetocheck):
+    commercialzonecodes = [
+        "N-1C",
+        "N-2C",
+        "N-2E",
+        "N-3C",
+        "N-3E",
+        "D-S",
+        "D-C"
+    ]
+    if(codetocheck in commercialzonecodes):
+        return True
+    else:
+        return False
+
+def generatescorefromaddress(hono, street, businessType):
+    addressInfo = opendatabuffalo.get(
+        "4eg6-xiba",
+        content_type="json",
+        hsenofr = hono,
+        street = street
+    )
+    if(len(addressInfo) == 1):
+        zoneCode = addressInfo[0]['plctypfut3']
+        if checkifzonedforcommercial(zoneCode):
+
+            return calculateNeighbooringBusinesses(hono, street, 5, businessType)
+        
+        else:
+            return 0
+    else:
+        return -1
+
+def calculateNeighbooringBusinesses(no, street, spread, businessType):
+    similarBusinesses = 0
+    for hono in range((no-spread), (no+spread+1)):
+        existingBusiness = opendatabuffalo.get(
+            "qcyy-feh8",
+            address = str(hono) + " " + street,
+            descript = businessType
+        )
+        similarBusinesses += len(existingBusiness)
+    
+    return similarBusinesses
